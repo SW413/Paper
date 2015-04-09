9406675ccd7a627fcdaaadca9dc7e5ec1e5341e9
mathiassmichno <mmichn13@student.aau.dk>
2015-04-09 11:40:30 +0200

    ppl never use this

--- delete_junk+pdf.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-call delete_junk.bat
-del *.synctex.gz
-echo Deleting PDF too!
-del *.pdf
-echo Boom! Gone!
--- delete_junk+pdf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sh delete_junk.sh
-rm -f *.synctex.gz
-echo "Deleting PDF too!"
-rm -f *.pdf
-echo "Boom! Gone!!"
