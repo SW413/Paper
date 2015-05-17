91e43be19d8600825b4ab0414abf7fb8f4e04783
Mathias Sass Michno <mmichn13@student.aau.dk>
2015-05-17 23:52:10 +0200

    graphs

---
+++ figures/tests/gnuplotTIKZ.s
@@ -0,0 +1,8 @@
+set term tikz
+set output 'graph.tikz'
+set xlabel 'Number of entries in result matrix'
+set ylabel 'Time in seconds'
+set title 'Execution Time Test'
+set grid
+plot 'cpu.data' using 1:2 with line t 'CPU' lw 1, 'gpu.data' using 1:2 with line t 'GPU' lw 1
+replot
