15c7957229d86eec14242eb38023b3534544e337
mathiassmichno <mmichn13@student.aau.dk>
2015-05-18 10:22:30 +0200

    lol

--- figures/tests/gnuplotTIKZ.s
+++
@@ -1,8 +0,0 @@
-set term tikz
-set output 'graph.tikz'
-set xlabel 'Number of entries in result matrix'
-set ylabel 'Time in seconds'
-set title 'Execution Time Test'
-set grid
-plot 'cpu.data' using 1:2 with line t 'CPU' lw 1, 'gpu.data' using 1:2 with line t 'GPU' lw 1
-replot
