5539caf32246adb438d19fd4060518b20681a056
mathiassmichno <mmichn13@student.aau.dk>
2015-05-20 10:47:36 +0200

    fukc af t

--- figures/tests/gnuplotPDF.s
+++ figures/tests/gnuplotPDF.s
@@ -3,5 +3,13 @@ set output 'graph.pdf'
 set xlabel 'Number of entries in result matrix'
 set ylabel 'Time in seconds'
 set title 'Execution Time Test'
-set grid
-plot 'cpu.data' using 1:2 with line t 'CPU' lw 1, 'gpu.data' using 1:2 with line t 'GPU' lw 1
+set grid 
+set key left
+f(x)=a*x**b;
+fit f(x) 'cpu.data' via a,b
+g(x)=c*x+d;
+fit g(x) 'gpu.data' via c,d 
+plot 'cpu.data' using 1:2 title ' CPU', \
+    f(x) with lines notitle, \
+    'gpu.data' using 1:2 title ' GPU', \
+    g(x) with lines notitle
